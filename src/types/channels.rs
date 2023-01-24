6d0a7b008bbb591edf8c09218e17543738548d77
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-24 12:24:49 +0000

    channel struct funcs

--- src/types/channels.rs
+++ src/types/channels.rs
@@ -19,6 +19,16 @@ pub enum ChannelType {
 pub struct Project {}
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct ChannelToken {
+    pub id: String,
+    pub created_at: String,
+    pub state: HashMap<String, String>,
+    pub expires_at: Option<String>,
+    pub is_online: bool,
+    // pub is_online: Option<bool>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Channel {
     pub id: String,
     // pub project: Project,
@@ -29,12 +39,29 @@ pub struct Channel {
     pub channel_type: ChannelType,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ChannelToken {
-    pub id: String,
-    pub created_at: String,
-    pub state: HashMap<String, String>,
-    pub expires_at: Option<String>,
-    pub is_online: bool,
-    // pub is_online: Option<bool>,
+impl Channel {
+    pub fn set_state(&self) {
+        println!("Setting channel state");
+        panic!("not implemented!")
+    }
+
+    pub fn patch_state(&self) {
+        println!("Patching channel state");
+        panic!("not implemented!")
+    }
+
+    pub fn subscribe_tokens(&self) {
+        println!("Subscribing tokens to channel");
+        panic!("not implemented!")
+    }
+
+    pub fn subscribe_token(&self) {
+        println!("Subscribing token to channel");
+        panic!("not implemented!")
+    }
+
+    pub fn publish_message(&self) {
+        println!("Publishing a channel message");
+        panic!("not implemented!")
+    }
 }
