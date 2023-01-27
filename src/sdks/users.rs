2e28fed28bbb502203248988ac508382b99835fe
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-27 11:29:15 +0000

    move user types

--- src/sdks/users.rs
+++ src/sdks/users.rs
@@ -2,26 +2,7 @@ use serde::Deserialize;
 
 use crate::client::APIClient;
 use crate::types::APIError;
-
-#[derive(Deserialize, Debug)]
-pub struct User {
-    pub id: String,
-    pub name: String,
-    pub email: String,
-    pub username: String,
-    pub email_verified: bool,
-    pub mfa_enabled: bool,
-    pub totp_enabled: bool,
-    pub webauthn_enabled: bool,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Pat {
-    pub id: String,
-    pub pat: String,
-    pub created_at: String,
-    pub name: Option<String>,
-}
+use crate::types::users::{Pat, User};
 
 pub struct Users {
     pub token: String,
--- src/types/users.rs
+++ src/types/users.rs
@@ -1,2 +1,21 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: String,
+    pub name: String,
+    pub email: String,
+    pub username: String,
+    pub email_verified: bool,
+    pub mfa_enabled: bool,
+    pub totp_enabled: bool,
+    pub webauthn_enabled: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Pat {
+    pub id: String,
+    pub pat: String,
+    pub created_at: String,
+    pub name: Option<String>,
+}
