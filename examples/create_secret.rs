d31ada20a881db1352ac8bb9018953984e5a79be
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-29 19:44:37 +0000

    example changes

--- examples/create_secret.rs
+++ examples/create_secret.rs
@@ -13,13 +13,15 @@ use hop::Hop;
 async fn main() {
     dotenv().ok();
 
-    // let my_token = "pat_xxx";
-    let my_token = env::var("PERSONAL_TOKEN").expect("PERSONAL_TOKEN needed!");
+    // let my_token = "ptk_xxx";
+    let my_token = env::var("PROJECT_TOKEN").expect("PROJECT_TOKEN needed!");
     let hop = Hop::new(my_token.as_str());
 
     // Example: Creating a project secret
-    hop.projects.create_secret(
+    let secret = hop.projects.create_secret(
         "RANDOM_NUMBER",
         rand::thread_rng().gen_range(0..100).to_string(),
     ).await.unwrap();
+
+    println!("secret: {:#?}", secret);
 }
