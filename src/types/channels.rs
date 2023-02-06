95a9614149007f7403600b5481f4a3538631631b
Tom Heaton <thomas.m.heaton@gmail.com>
2023-02-06 21:53:29 +0000

    channel struct docstrings

--- src/types/channels.rs
+++ src/types/channels.rs
@@ -21,21 +21,33 @@ pub struct Project {}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ChannelToken {
+    /// The ID for the token
     pub id: String,
-    pub created_at: String,
+    /// State for this token
     pub state: HashMap<String, String>,
-    pub expires_at: Option<String>,
+    // TODO: check this
+    // /// The project this channel token is associated with
+    // project_id: String,
+    /// If this token is currently online (e.g. active heartbeat and connected to leap)
     pub is_online: bool,
-    // pub is_online: Option<bool>,
+    // TODO: check these
+    pub expires_at: Option<String>,
+    pub created_at: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Channel {
+    /// The ID of the channel
     pub id: String,
+    // /// The project it is associated with
     // pub project: Project,
+    /// State metadata
     pub state: HashMap<String, String>,
+    // /// Capabilities of the channel
     // pub capabilities: i64,
+    /// When this channel was created
     pub created_at: String,
+    /// The type of this channel
     #[serde(rename = "type")]
     pub channel_type: ChannelType,
 }
